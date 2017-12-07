33dd6ab01cea7a2a83d3d9d0c7682f716cbcb8b2
Bruce Becker <brucellino@gmail.com>
2017-12-07 05:59:35 +0100

    Fix lint errors in tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,28 +7,32 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_hosts_file(host):
+    """Basic checks on the host."""
     f = host.file('/etc/hosts')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
 
+
 def test_cvmfs_client(host):
-    """Test that the CVMFS client is properly installed"""
+    """Test that the CVMFS client is properly installed."""
     pkg = host.package('cvmfs')
     client = host.file('/usr/bin/cvmfs2')
     version = '2.4.3'
 
     assert pkg.is_installed
     assert pkg.version.startswith(version)
+    assert client.exists
 
 def test_CODE_RADE_mounted(host):
     """Check that the CODE-RADE repo is mounted"""
 
     assert host.mount_point("/cvmfs/code-rade.africa-grid.org").exists
 
+
 def test_CODE_RADE_version(host):
-    """Check CODE-RADE version"""
+    """Check CODE-RADE version."""
 
     cvmfs_version = host.file('/cvmfs/code-rade.africa-grid.org/version')
     assert cvmfs_version.exists
